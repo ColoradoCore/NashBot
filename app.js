03cb0bdb356967b8e5b549d9766a9a7300bcb9fe
Wiemanizer <brycewieman@gmail.com>
2018-03-09 14:19:41 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+
+//Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+  console.log('%s listening to %s', server.name, server.url);
+});
+
+//Create chat connector for commmunicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+  appID: process.env.MicrosoftAppID,
+  appPassword: process.env.MicrosoftAppPassword
+});
+
+//Listen for messages from users
+server.post('api/messages', connector.listen());
+
+var inMemoryStorage = new builder.MemoryBotStorage();
+
+// This is a dinner reservation bot that uses a waterfall technique to prompt users for input.
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send("Welcome to the dinner reservation.");
+        builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
+    },
+    function (session, results) {
+        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+        builder.Prompts.text(session, "How many people are in your party?");
+    },
+    function (session, results) {
+        session.dialogData.partySize = results.response;
+        builder.Prompts.text(session, "Who's name will this reservation be under?");
+    },
+    function (session, results) {
+        session.dialogData.reservationName = results.response;
+
+        // Process request and display reservation details
+        session.send(`Reservation confirmed. Reservation details: <br/>Date/Time: ${session.dialogData.reservationDate} <br/>Party size: ${session.dialogData.partySize} <br/>Reservation name: ${session.dialogData.reservationName}`);
+        session.endDialog();
+    }
+]).set('storage', inMemoryStorage); // Register in-memory storage 
